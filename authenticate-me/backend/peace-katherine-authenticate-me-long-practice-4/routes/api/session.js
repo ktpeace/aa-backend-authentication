2ebe6036fd1266ab50687c481ec2e75ce11bba3d
ktpeace <katpeace@protonmail.com>
2022-09-01 15:23:58 -0400

    added Express request body validations

--- authenticate-me/backend/peace-katherine-authenticate-me-long-practice-4/routes/api/session.js
+++ authenticate-me/backend/peace-katherine-authenticate-me-long-practice-4/routes/api/session.js
@@ -6,25 +6,9 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
-const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
-
-// validate login format
-const validateLogin = [
-  check('credential')
-    .exists({ checkFalsy: true })
-    .notEmpty()
-    .withMessage('Please provide a valid email or username.'),
-  check('password')
-    .exists({ checkFalsy: true })
-    .withMessage('Please provide a password.'),
-  handleValidationErrors
-];
-
 // Log in
 router.post(
   '/',
-  validateLogin,
   async (req, res, next) => {
     const { credential, password } = req.body;
 
@@ -68,6 +52,5 @@ router.get(
     } else return res.json({});
   }
 );
-
 
 module.exports = router;
